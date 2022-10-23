3d71a68a833efae2d0c10a80f8a974da39ff0882
SPriyan0108 <116474109+SPriyan0108@users.noreply.github.com>
2022-10-23 09:37:05 +0530

    2_Syso Added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,12 @@
-public class HelloWorld{
+public class HelloWorld {
 
  public static void main(String[] args) {
   // TODO Auto-generated method stub
     System.out.println("Project1");
     System.out.println("Project2");
-
+    System.out.println("Project3");
+    System.out.println("Project3");
+    
  }
 
 }
