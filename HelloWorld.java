f9ab94618fa22965bfd66deef0d750f521d96670
SPriyan0108 <116474109+SPriyan0108@users.noreply.github.com>
2022-10-23 09:09:58 +0530

    Syso Included Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld{
 
  public static void main(String[] args) {
   // TODO Auto-generated method stub
+    System.out.println("Project1");
     System.out.println("Project2");
 
  }
